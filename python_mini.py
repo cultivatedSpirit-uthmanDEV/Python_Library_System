46dfba0cb1380f837d7438ff6abadf047d31ee46
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2025-11-26 20:11:00 +0100

    Available_Book

--- python_mini.py
+++ python_mini.py
@@ -15,5 +15,21 @@ def add_book(title, author, year):
   print(f"Book '{title}' added to library.")
 
 add_book( title= "The journey to west", author= "J. Uthman", year= "1956")
+
+
+## Available Book in the library
+
+def available_book():
+   for Avail_Book in Books:
+    print("Available Book: " f"{Avail_Book['title']} by ({Avail_Book['author']}) ({Avail_Book['year']})")
+
+
+available_book()
+
+
+
+
+
+
   
 
