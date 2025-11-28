a12318b78b6b2944a03ff814e84038be136c174d
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2025-11-28 07:11:10 +0100

    search_book and  borrow_Book

--- python_mini.py
+++ python_mini.py
@@ -14,10 +14,19 @@ def add_book(title, author, year):
   Books.append(Book)
   print(f"Book '{title}' added to library.")
 
-add_book( title= "The journey to west", author= "J. Uthman", year= "1956")
+add_book( title= "Python for begonners", author= "NoorLab institute", year= "2025")
+add_book( title= "The journey to west", author= "J. chenyu", year= "1956")
+add_book( title= "futurist peeps", author= "W. Uthman", year= "1956")
 
 
 ## Available Book in the library
+
+print("      ")
+print("      ")
+print("Available Book in the library")
+print("      ")
+
+
 
 def available_book():
    for Avail_Book in Books:
@@ -26,6 +35,43 @@ def available_book():
 
 available_book()
 
+## Book search
+
+print("      ")
+print("      ")
+print("Book Search")
+print("      ")
+
+
+
+def search_book(title):
+  for Book in Books:
+    if Book['title'].lower() == title.lower():
+      print(f"Found: {Book['title']} by {Book['author']} ({Book['year']})")
+      return
+  print("Book not found")
+
+search_book( "futurist peeps")
+
+
+## Borrowed book
+
+print("      ")
+print("      ")
+print("Borrow Book")
+print("      ")
+
+
+def borrow_Book(title):
+  for Book in Books:
+    if Book['title'].lower() == title.lower():
+      Books.remove(Book)
+      print(f"{Book['title']} has been borrowed out")
+      return
+  print('Book not found')
+
+
+borrow_Book('The journey to west')
 
 
 
